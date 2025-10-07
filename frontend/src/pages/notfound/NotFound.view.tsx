14e1d3e9b2177d3a46e6321d4020c62fe273a46f
eshet022 <eshet022@umn.edu>
2025-10-07 14:25:29 -0500

    update not found view, add notfound styles

--- frontend/src/pages/notfound/NotFound.view.tsx
+++ frontend/src/pages/notfound/NotFound.view.tsx
@@ -1,10 +1,30 @@
+import './NotFound.css'
+import {useNavigate} from "react-router";
+
 /**
- * Page not found :(
+ * Page not Found
+ *
+ * @author AJ Kneisl, Yordanos Eshete
  */
 export default function NotFound() {
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        if (window.history.length > 1) {
+            navigate(-1);
+        }
+        else { navigate('/umn.app', { replace: true })
+        }
+    }
+
     return (
-        <div className="flex flex-fow justify-center items-center mt-4">
-            <h1 className="text-2xl figtree">Page Not Found</h1>
+        <div className="fire">
+            <h1 className="text">404</h1>
+            <h4 className="word">Page not found</h4>
+            <div className="btn-wrapper hover:cursor-pointer" onClick={handleGoBack}>
+
+                <button className="back-btn" >Go Back</button>
+            </div>
+            <button className="btn">Report a Problem</button>
         </div>
     )
 }
