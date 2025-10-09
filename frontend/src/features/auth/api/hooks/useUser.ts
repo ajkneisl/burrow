5ceaa91b2b6163d7300dc9c5746d703fd32274ca
AJ Kneisl <aj@ajkneisl.dev>
2025-10-08 22:42:21 -0500

    redirect to welcome when the token is reset

--- frontend/src/features/auth/api/hooks/useUser.ts
+++ frontend/src/features/auth/api/hooks/useUser.ts
@@ -3,12 +3,14 @@ import { useAtom } from "jotai"
 import { authToken } from "../auth.atom.ts"
 import { useQuery } from "@tanstack/react-query"
 import { getUser } from "@features/auth/api/user.api.ts"
+import { useNavigate } from "react-router"
 
 /**
  * Retrieve the `User` object.
  */
 export default function useUser(): User | null {
     const [auth, setAuth] = useAtom(authToken)
+    const nav = useNavigate()
 
     const { data, error } = useQuery({
         queryKey: ["user"],
@@ -19,6 +21,7 @@ export default function useUser(): User | null {
     // if the request fails, log the user out
     if (auth !== "" && error) {
         setAuth("")
+        nav("/welcome")
         return null
     }
 
