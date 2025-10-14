35b52a42d0864ae83034e1316e37c889cc252873
AJ Kneisl <aj@ajkneisl.dev>
2025-10-14 17:15:11 -0500

    update bookmark to share theme styles

--- frontend/src/pages/meetings/overview/component/BookmarkMeeting.tsx
+++ frontend/src/pages/meetings/overview/component/BookmarkMeeting.tsx
@@ -1,9 +1,13 @@
-import {useQueryClient} from "@tanstack/react-query";
-import {createBookmark, deleteBookmark} from "@features/groups/api/groups.api.ts";
-import useToken from "@features/auth/api/hooks/useToken.ts";
+import { useQueryClient } from "@tanstack/react-query"
+import {
+    createBookmark,
+    deleteBookmark
+} from "@features/groups/api/groups.api.ts"
+import useToken from "@features/auth/api/hooks/useToken.ts"
+import clsx from "clsx"
 
 type BookmarkMeetingProps = {
-    isBookmarked: boolean,
+    isBookmarked: boolean
     meetingId: string
 }
 
@@ -14,17 +18,19 @@ type BookmarkMeetingProps = {
  * @param meetingId The ID of the meeting.
  * @constructor
  */
-export default function BookmarkMeeting({isBookmarked, meetingId}: BookmarkMeetingProps) {
+export default function BookmarkMeeting({
+    isBookmarked,
+    meetingId
+}: BookmarkMeetingProps) {
     const queryClient = useQueryClient()
     const auth = useToken()
 
     async function bookmark() {
-        if (auth === null)
-            return
+        if (auth === null) return
 
         queryClient.setQueryData(["meeting", meetingId], (old: any) => {
             if (!old) return old
-            return {...old, bookmarked: !old.bookmarked}
+            return { ...old, bookmarked: !old.bookmarked }
         })
 
         if (!isBookmarked) {
@@ -34,20 +40,24 @@ export default function BookmarkMeeting({isBookmarked, meetingId}: BookmarkMeeti
         }
     }
 
-    return <button
-        className={`cursor-pointer inline-flex items-center justify-center rounded-xl border border-gray-200 bg-white p-2 text-sm font-medium shadow-sm transition hover:shadow-md ${isBookmarked ? "text-blue-500" : "text-gray-500"}`}
-        aria-label="Bookmark"
-        title="Bookmark"
-        onClick={bookmark}
-    >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="h-5 w-5"
+    return (
+        <button
+            className={clsx(
+                `cursor-pointer inline-flex items-center justify-center rounded-xl border bg-background p-2 text-sm font-medium shadow-sm transition hover:shadow-md`,
+                isBookmarked ? "text-secondary" : "text-text"
+            )}
+            aria-label="Bookmark"
+            title="Bookmark"
+            onClick={bookmark}
         >
-            <path
-                d="M6.32 2.75A2.25 2.25 0 0 0 4.25 5v16a.75.75 0 0 0 1.2.6l6.33-4.75 6.33 4.75a.75.75 0 0 0 1.2-.6V5A2.25 2.25 0 0 0 17.68 2.75H6.32z"/>
-        </svg>
-    </button>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-5 w-5"
+            >
+                <path d="M6.32 2.75A2.25 2.25 0 0 0 4.25 5v16a.75.75 0 0 0 1.2.6l6.33-4.75 6.33 4.75a.75.75 0 0 0 1.2-.6V5A2.25 2.25 0 0 0 17.68 2.75H6.32z" />
+            </svg>
+        </button>
+    )
 }
