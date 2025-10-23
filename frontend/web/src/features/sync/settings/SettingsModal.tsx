37b8e225a9977eeaa708cdbb423fffafb2b093fa
eshet022 <eshet022@umn.edu>
2025-10-23 16:11:09 -0500

    update not found view, add notfound styles

--- frontend/web/src/features/sync/settings/SettingsModal.tsx
+++ frontend/web/src/features/sync/settings/SettingsModal.tsx
@@ -53,7 +53,9 @@ export default function SettingsModal() {
             title="Settings"
             widthClass="max-w-3xl"
             footer={
+
                 <div className="w-full flex items-center justify-between">
+
                     <Button>Delete my Account</Button>
 
                     <div className="flex items-center gap-2">
@@ -82,9 +84,13 @@ export default function SettingsModal() {
                         "flex sm:flex-col flex-row gap-2 -mt-1 mb-3 p-1 mr-8"
                     )}
                 >
-                    <SettingsNavigationButton name="Account" />
-                    <SettingsNavigationButton name="Notifications" />
-                    <SettingsNavigationButton name="Theme" />
+                    <SettingsNavigationButton name="Account" icon={<p><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 14C7.86 14 4 17.13 4 21H20C20 17.13 16.14 14 12 14Z" fill="currentColor"/>
+                    </svg></p>} />
+                    <SettingsNavigationButton name="Notifications" icon={<p><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 22C13.1 22 14 21.1 14 20H10C10 21.1 10.9 22 12 22ZM18 16V11C18 7.93 16.36 5.36 13.5 4.68V4C13.5 3.17 12.83 2.5 12 2.5C11.17 2.5 10.5 3.17 10.5 4V4.68C7.63 5.36 6 7.92 6 11V16L4 18V19H20V18L18 16Z" fill="currentColor"/>
+                    </svg></p>} />
+                    <SettingsNavigationButton name="Theme" icon={<p> </p>}/>
                 </div>
 
                 <div className="flex min-h-80 flex-1 gap-4 flex-col">
