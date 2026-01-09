0c375d5926744a5329b9efe1af8ab289d610bf3c
AJ Kneisl <aj@ajkneisl.dev>
2026-01-09 11:30:10 -0600

    change splash icon to burrow

--- frontend/app/app.config.ts
+++ frontend/app/app.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
     splash: {
-        image: "./assets/images/splash-icon.png",
+        image: "./assets/images/burrow.png",
         resizeMode: "contain",
         backgroundColor: "#7A0019"
     },
@@ -83,7 +83,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         [
             "expo-splash-screen",
             {
-                image: "./assets/images/splash-icon.png",
+                image: "./assets/images/burrow.png",
                 imageWidth: 200,
                 resizeMode: "contain",
                 backgroundColor: "#7A0019",
