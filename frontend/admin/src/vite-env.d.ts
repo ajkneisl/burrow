2fff23b7d7147a99451b2abacc4a98391387ff67
AJ Kneisl <aj@ajkneisl.dev>
2026-07-28 21:55:16 -0500

    feat(frontend): update web icon depending on stage

--- frontend/admin/src/vite-env.d.ts
+++ frontend/admin/src/vite-env.d.ts
@@ -2,10 +2,8 @@
 
 interface ImportMetaEnv {
     readonly VITE_APP_ENV: "dev" | "staging" | "prod"
-    readonly VITE_VERSION: string
     readonly VITE_BASE_URL: string
     readonly VITE_CDN_URL: string
-    readonly VITE_GMAP_API_KEY: string
     readonly VITE_GOOGLE_CLIENT_ID: string
 }
 
